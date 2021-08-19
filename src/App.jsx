8b0f1e09196f60951e4cdf2c3aa2cd5ea5349139
Julija Faltina <dashinicha@gmail.com>
2021-08-19 11:10:07 +0300

    Added contacts form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 // import React, { Component } from "react";
 // import ContactsList from "./components/ContactsList/ContactsList";
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
+import ContactForm from './components/ContactForm/ContactForm';
     
     
     
 function App() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm />
+      </div>
+    );
 
 }
 
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,12 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ContactForm extends Component {
   state = {
-      name: "",
-      number: "",
+    name: "",
+    number: "",
   };
-    onInputChange
-    
+  onInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmitData(this.state.name, this.state.number);
+    this.reset();
+  };
+  reset = () => {
+    this.setState({ name: "", number: "" });
+  };
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <label>
+          <span>Name</span>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+            value={name}
+            onChange={this.onInputChange}
+          />
+        </label>
+        <label>
+          <span>Number</span>
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+                    required
+                    value={number}
+                    onChange={this.onInputChange}
+          />
+            </label>
+            <button type = 'submit'>Add contact</button>
+      </form>
+    );
+  }
 }
-
-
+export default ContactForm;
